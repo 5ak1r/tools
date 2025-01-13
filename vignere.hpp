ac3fb5be84fafdd7e5e441664e145c31c7ab0886
Rikas <s.azimkar1@newcastle.ac.uk>
2025-01-13 18:08:04 +0000

    removed exe and added vignere header

---
+++ vignere.hpp
@@ -0,0 +1,6 @@
+#include <string>
+
+std::string v_fix_key(std::string text, std::string key);
+
+std::string v_encrypt(std::string plaintext, std::string key);
+std::string v_decrypt(std::string ciphertext, std::string key);
