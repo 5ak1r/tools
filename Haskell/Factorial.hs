bb827fc7fbd22cf81d459edbcd01bf7cd5aef44a
Rikas <s.azimkar1@newcastle.ac.uk>
2025-01-28 15:48:33 +0000

    need to look up string formatting

--- Haskell/Factorial.hs
+++ Haskell/Factorial.hs
@@ -1,7 +1,8 @@
 fact :: Int -> Int
-fact 0 = 1
-fact n = n * fact(n - 1)
+fact n | n == 0 = 1
+       | n /= 0 = n * fact (n - 1)
 
+main :: IO()
 main = do
-    putStrLn "The factorial of 5 is:"
-    print (fact 5)
+    putStrLn "The factorial of 10 is"
+    print (fact 10)
